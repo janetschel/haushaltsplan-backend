81a799238e55d991250be3d45ee0b074263ae91c
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-29 14:41:11 +0200

    Changed ResponseEntity to String

--- src/main/java/com/heroku/backend/controller/HealthCheckController.java
+++ src/main/java/com/heroku/backend/controller/HealthCheckController.java
@@ -1,7 +1,6 @@
 package com.heroku.backend.controller;
 
 import com.heroku.backend.service.HealtCheckService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +13,7 @@ public class HealthCheckController {
     }
 
     @GetMapping("/healthcheck")
-    public ResponseEntity<String> healthCheck() {
+    public String healthCheck() {
         return healtCheckService.healthCheck();
     }
 }
--- src/main/java/com/heroku/backend/service/HealtCheckService.java
+++ src/main/java/com/heroku/backend/service/HealtCheckService.java
@@ -1,6 +1,5 @@
 package com.heroku.backend.service;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
