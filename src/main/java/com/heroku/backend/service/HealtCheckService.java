65cc25c78762500d3e3d9da3bc171d114bd5fb48
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-29 14:40:03 +0200

    Update HealtCheckService.java

--- src/main/java/com/heroku/backend/service/HealtCheckService.java
+++ src/main/java/com/heroku/backend/service/HealtCheckService.java
@@ -3,11 +3,18 @@ package com.heroku.backend.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class HealtCheckService {
 
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("Healthcheck completed");
+    public String healthCheck() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        String response = "Healthcheck completed at " + dateTimeFormatter.format(localDateTime);
+        return response;
     }
 
 }
