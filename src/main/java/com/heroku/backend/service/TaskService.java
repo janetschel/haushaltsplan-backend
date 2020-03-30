6a8d5982c8438a9a9ab3c2b15f13e29effc0b35b
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-30 14:03:28 +0200

    Imtplemented service and validation in backend

--- src/main/java/com/heroku/backend/service/TaskService.java
+++ src/main/java/com/heroku/backend/service/TaskService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +29,65 @@ public class TaskService {
 
     public ResponseEntity<List<TaskEntity>> getDocuments() {
         return ResponseEntity.ok(taskRepository.findAll());
+    }
+
+    public ResponseEntity<String> addDocument(TaskEntity taskEntity) {
+        String id = taskEntity.getId();
+        id = id == null ? "0" : id;
+
+        String message = "Task with ID '" + id + "' does already exist and could therefore not be created";
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(id));
+        TaskEntity documentToAdd = mongoOperations.findOne(query, TaskEntity.class);
+
+        if (documentToAdd != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+        }
+
+        taskRepository.insert(taskEntity);
+
+        message = "Task added successfully";
+        return ResponseEntity.status(HttpStatus.CREATED).body(message);
+    }
+
+    public ResponseEntity<String> updateDocument(TaskEntity taskEntity) {
+        String id = taskEntity.getId();
+        String message =
+                "Task with ID '" + id + "' does not exist and could therefore not be updated. It was created instead";
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(id));
+        TaskEntity documentToUpdate = mongoOperations.findOne(query, TaskEntity.class);
+
+        if (documentToUpdate == null) {
+            addDocument(taskEntity);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(message);
+        }
+
+        documentToUpdate.setBlame(taskEntity.getBlame());
+        documentToUpdate.setPic(taskEntity.getPic());
+        documentToUpdate.setDone(taskEntity.isDone());
+        mongoOperations.save(documentToUpdate);
+
+        message = "Task updated successfully";
+        return ResponseEntity.ok(message);
+    }
+
+    public ResponseEntity<String> deleteDocument(String id) {
+        String message = "Task with ID '" + id + "' does not exist and could therefore not be deleted";
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(id));
+        TaskEntity documentToDelete = mongoOperations.findOne(query, TaskEntity.class);
+
+        if (documentToDelete == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
+
+        mongoOperations.remove(documentToDelete);
+
+        message = "Task deleted successfully";
+        return ResponseEntity.ok(message);
     }
 }
