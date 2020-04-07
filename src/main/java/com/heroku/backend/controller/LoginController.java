a68a98454153e3be2107ab3837bfb523ec93016a
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-04-07 09:11:22 +0200

    Trying again

--- src/main/java/com/heroku/backend/controller/LoginController.java
+++ src/main/java/com/heroku/backend/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://haushaltsplan.herokuapp.com/", maxAge = 3600)
 @RestController
 public class LoginController {
     public final LoginService loginService;
