12c3c1edb618a6e5dd6c7fa9a7a80ba0bf5c203b
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-04-07 19:48:13 +0200

    Removed some tests
    Removed tests that don't make much sense. I was mocking the service and testing the mock, not the service itself. welp

--- src/test/java/com/heroku/backend/service/TaskServiceTest.java
+++ src/test/java/com/heroku/backend/service/TaskServiceTest.java
@@ -3,16 +3,12 @@ package com.heroku.backend.service;
 import com.heroku.backend.repository.TaskRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TaskServiceTest {
-    @InjectMocks
-    private TaskService taskService;
-
     @Mock
     private TaskRepository taskRepository;
 
