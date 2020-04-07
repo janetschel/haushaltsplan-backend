df4b63d91b5b48e6d9f74e516336d2d8fe6e415f
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-04-07 10:31:07 +0200

    Update AuthTokenService.java
    Renaming effects of ValueConfiguration

--- src/main/java/com/heroku/backend/service/AuthTokenService.java
+++ src/main/java/com/heroku/backend/service/AuthTokenService.java
@@ -1,6 +1,6 @@
 package com.heroku.backend.service;
 
-import com.heroku.backend.ValueConfiguration;
+import com.heroku.backend.config.ValueConfig;
 import com.heroku.backend.exception.InvalidBase64StringException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Service;
 public class AuthTokenService {
 
     public ResponseEntity<String> getAuthToken(String base64String) throws InvalidBase64StringException {
-        if (base64String.equals(ValueConfiguration.base64FirstUser) ||
-                base64String.equals(ValueConfiguration.base64SecondUser)) {
-            return ResponseEntity.ok(ValueConfiguration.authenticationToken);
+        if (base64String.equals(ValueConfig.base64FirstUser) ||
+                base64String.equals(ValueConfig.base64SecondUser)) {
+            return ResponseEntity.ok(ValueConfig.authenticationToken);
         }
 
         throw new InvalidBase64StringException();
