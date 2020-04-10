2579819571159cd98b7b63070b03cf9fb8b81d92
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-04-10 12:54:43 +0200

    Added logging for debugging purposes on deployment

--- src/main/java/de/janetschel/haushaltsplan/backend/service/LoginService.java
+++ src/main/java/de/janetschel/haushaltsplan/backend/service/LoginService.java
@@ -1,10 +1,12 @@
 package de.janetschel.haushaltsplan.backend.service;
 
+import de.janetschel.haushaltsplan.backend.App;
 import de.janetschel.haushaltsplan.backend.config.ValueConfig;
 import de.janetschel.haushaltsplan.backend.exception.TooManyRequestsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Service
 @EnableScheduling
 public class LoginService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginService.class);
 
     private static int attempts = 0;
 
@@ -30,6 +33,14 @@ public class LoginService {
 
     @Scheduled(cron = "*/20 * * * * *")
     private void resetAttempts() {
+        LOGGER.info("Reset attempts for login. Accepting all requests now.");
+
+        if (attempts >= 10) {
+            LOGGER.warn(attempts + " attempts made since the last reset.");
+        } else {
+            LOGGER.info(attempts + " attempts made since the last reset. No indication of any attacks.");
+        }
+
         attempts = 0;
     }
 }
